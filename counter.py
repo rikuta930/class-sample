87de617acd7eed75ef82bf674e6a0a3f4b235eef
rimurun <rimuru930@gmail.com>
2020-08-20 01:03:14 +0900

    チェック終了｡良きコードに変化した｡

--- counter.py
+++ counter.py
@@ -1,13 +1,14 @@
 class Counter:
-    def __init__(self, value):
+    def __init__(self, value,step):
         self.value = value
+        self.step = step
 
     def increase(self):
-        self.value += 1
+        self.value += self.step
 
 
 if __name__ == '__main__':
-    counter_1 = Counter(0)
+    counter_1 = Counter(0,1)
     print(counter_1.value)
 
     counter_1.increase()
@@ -16,5 +17,8 @@ if __name__ == '__main__':
     counter_1.increase()
     print(counter_1.value)
 
-    counter_2 = Counter(1)
+    counter_2 = Counter(1,4)
+    print(counter_2.value)
+
+    counter_2.increase()
     print(counter_2.value)
