e7f189d3f090268b5c7cec6ed6d4b2affbcc05c0
rimurun <rimuru930@gmail.com>
2020-08-19 21:51:54 +0900

    インスタンス変数を学んだ

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 class User:
     def __init__(self, name, age, occupation):
+        # インスタンス変数
         self.name = name
         self.age = age
         self.occupation = occupation
@@ -7,17 +8,7 @@ class User:
 
 if __name__ == '__main__':
     ross = User('Ross', 30, 'paleontologist')  # Userクラスをインスタンス化
-    print(ross)
-    print(ross.name)
-    print(ross.age)
-    print(ross.occupation)
 
     joey = User("Joey", 31, 'actor')
-    print(joey)
-    print(joey.name)
-    print(joey.occupation)
 
     chandler = User("Chandler", 31, '???')
-    print(chandler)
-    print(chandler.name)
-    print(chandler.occupation)
