dc0016c026598a3e735c1e1a88fb7ada13e1e7bb
rimurun <rimuru930@gmail.com>
2020-08-19 22:23:00 +0900

    インスタンスメソッドを追加
    簡単っぽい イカおいしい

--- user.py
+++ user.py
@@ -5,10 +5,22 @@ class User:
         self.age = age
         self.occupation = occupation
 
+    def display_profile(self):
+        # display_profile() は Userクラスの インスタンスメソッド
+        print(f'{self.name}, 年齢:{self.age}歳, 職業:{self.occupation}')
+
+    def change_occupation(self, new_occupation):
+        self.occupation = new_occupation
+
 
 if __name__ == '__main__':
     ross = User('Ross', 30, 'paleontologist')  # Userクラスをインスタンス化
+    ross.display_profile()
 
     joey = User("Joey", 31, 'actor')
+    joey.display_profile()
 
     chandler = User("Chandler", 31, '???')
+    chandler.display_profile()
+    chandler.change_occupation('Statistical analysis‎')
+    chandler.display_profile()
